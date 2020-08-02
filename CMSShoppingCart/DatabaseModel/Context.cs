32be44720e1bf8e0984fceea5bdbcdeee3b9ad18
mads nielsen kjerumgaard <mads@mmm-m.dk>
2020-08-02 16:05:31 +0200

    Added Class Liberay for DataAccess and DatabaseModel

---
+++ CMSShoppingCart/DatabaseModel/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseModel
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context>  options) : base(options)
+        {
+        }
+
+
+    }
+}
